e3d8e180e80f3680d18c3c5fc53eb212777fe4c9
Kenley Dev <92249212+KenleyundLeon@users.noreply.github.com>
2025-05-30 06:46:42 +0200

    Update Events.cs
    Made the code like 150% better and more optimized

--- Events.cs
+++ Events.cs
@@ -1,37 +1,27 @@
-﻿
 using Interactables.Interobjects.DoorUtils;
-using InventorySystem;
-using InventorySystem.Items;
-using KcItem = InventorySystem.Items.Keycards.KeycardItem;
 using LabApi.Events.Arguments.PlayerEvents;
 using LabApi.Events.CustomHandlers;
-using LabApi.Features.Wrappers;
-using LabApi.Features.Console;
+using LAPI.Configs;
+using KcItem = InventorySystem.Items.Keycards.KeycardItem;
 
-namespace Wireless_Keycard;
-
-public class Events : CustomEventsHandler
+namespace Wireless_Keycards;
+public class WirelessCard : CustomEventsHandler
 {
     public override void OnPlayerInteractingDoor(PlayerInteractingDoorEventArgs ev)
     {
-        if (ev.Player == null || ev.Door == null) return;
-
-        Player ply = ev.Player;
-        InventoryInfo invInfo = ply.Inventory.UserInventory;
-        Door door = ev.Door;
-        if (door.Permissions == DoorPermissionFlags.None)
+        if (ev.Door.Permissions == DoorPermissionFlags.None)
         {
             ev.IsAllowed = true;
             ev.CanOpen = true;
             return;
         }
 
-        foreach (ItemBase item in invInfo.Items.Values)
+        foreach (var item in ev.Player.Inventory.UserInventory.Items.Values)
         {
-            if (item.Category != ItemCategory.Keycard) continue;
-            KcItem keycard = item as KcItem;
-            DoorPermissionFlags flags = keycard.GetPermissions(ply as IDoorPermissionRequester);
-            if (door.Base.RequiredPermissions.CheckPermissions(flags))
+            if (item is not KcItem keycard) continue;
+
+            var permissions = keycard.GetPermissions(ev.Player as IDoorPermissionRequester);
+            if (ev.Door.Base.RequiredPermissions.CheckPermissions(permissions))
             {
                 ev.IsAllowed = true;
                 ev.CanOpen = true;
@@ -42,87 +32,27 @@ public class Events : CustomEventsHandler
 
     public override void OnPlayerInteractingLocker(PlayerInteractingLockerEventArgs ev)
     {
-        string lockerName = ev.Locker.Base.name;
-        if (lockerName.Contains("MedkitStructure"))
+        foreach (var item in ev.Player.Inventory.UserInventory.Items.Values)
         {
-            ev.CanOpen = true;
-            ev.IsAllowed = true;
-            return;
-        }
-
-        foreach (Item item in ev.Player.Items)
-        {
-            if (item is not KeycardItem keycard) continue;
-
-            switch (keycard.Type)
+            if (item is not KcItem keycard) continue;
+            if (keycard.GetPermissions(ev.Player as IDoorPermissionRequester).HasFlagAll(ev.Chamber.RequiredPermissions))
             {
-                case ItemType.KeycardResearchCoordinator:
-                    if (!lockerName.Contains("PedestalStructure")) return;
-                    ev.CanOpen = true;
-                    ev.IsAllowed = true;
-                    break;
-
-                case ItemType.KeycardGuard:
-                    if (!lockerName.Contains("LargeGunLockerStructure")) return;
-                    ev.CanOpen = true;
-                    ev.IsAllowed = true;
-                    break;
-
-                case ItemType.KeycardMTFPrivate:
-                case ItemType.KeycardContainmentEngineer:
-                case ItemType.KeycardMTFOperative:
-                case ItemType.KeycardFacilityManager:
-                    if (!lockerName.Contains("PedestalStructure") && !lockerName.Contains("RifleRackStructure") && !lockerName.Contains("LargeGunLockerStructure")) return;
-                    ev.CanOpen = true;
-                    ev.IsAllowed = true;
-                    break;
-
-                case ItemType.KeycardMTFCaptain:
-                case ItemType.KeycardChaosInsurgency:
-                case ItemType.KeycardO5:
-                    if (!lockerName.Contains("PedestalStructure") && !lockerName.Contains("Experimental Weapon Locker") && !lockerName.Contains("RifleRackStructure") && !lockerName.Contains("LargeGunLockerStructure")) return;
-                    ev.CanOpen = true;
-                    ev.IsAllowed = true;
-
-                    break;
+                ev.IsAllowed = true;
+                ev.CanOpen = true;
+                return;
             }
         }
     }
-
     public override void OnPlayerInteractingGenerator(PlayerInteractingGeneratorEventArgs ev)
     {
-        foreach (Item item in ev.Player.Items)
+        foreach (var item in ev.Player.Inventory.UserInventory.Items.Values)
         {
-            if (item is not KeycardItem keycard) continue;
-
-            switch (keycard.Type)
+            if (item is not KcItem keycard) continue;
+            if (keycard.GetPermissions(ev.Player as IDoorPermissionRequester).HasFlagAll(ev.Generator.RequiredPermissions))
             {
-                case ItemType.KeycardMTFPrivate:
-                case ItemType.KeycardMTFOperative:
-                case ItemType.KeycardMTFCaptain:
-                case ItemType.KeycardChaosInsurgency:
-                case ItemType.KeycardO5:
-                    ev.IsAllowed = true;
-                    ev.Generator.IsUnlocked = true;
-                    break;
-            }
-        }
-    }
-
-    public override void OnPlayerUnlockingWarheadButton(PlayerUnlockingWarheadButtonEventArgs ev)
-    {
-        if (ev.Player == null) return;
-
-        foreach (Item item in ev.Player.Items)
-        {
-            if (item is not KeycardItem keycard) continue;
-
-            switch (keycard.Type)
-            {
-                case ItemType.KeycardFacilityManager:
-                case ItemType.KeycardO5:
-                    ev.IsAllowed = true;
-                    break;
+                ev.IsAllowed = true;
+                ev.Generator.IsUnlocked = true;
+                return;
             }
         }
     }
